64b27a7285d66cab9da434b25da56a8d9ca1fc3b
Karl Cabalquinto <krlcblqnt@gmail.com>
2024-11-09 11:45:56 +0800

    add model, model and migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,7 +17,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::create([
-            'email' => 'admin@admin.com',
+            'email' => 'admin@admin.Com',
             'password' => Hash::make('password'), 
         ]);
     }
